7bbfc69fc8dc87dff16ff400c1111ba4acbd078d
unknown <15094052912@163.com>
2021-05-13 15:26:42 +0800

    添加文件

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+/*
+ * @Autor: MapleLeaves
+ * @Date: 2021-05-12 22:32:29
+ * @LastEditors: MapleLeaves
+ * @LastEditTime: 2021-05-12 23:39:03
+ */
+const data:string = '我是大鑫';
+console.error(data)
+function getname(name:string):string {
+  return name
+}
+let nameOne = getname("daxin");
+console.error(nameOne)
+enum Days  {
+  Sun,
+  Mon,
+  Tue,
+  Wen,
+  Thu,
+  Fri,
+  Sat
+}
+interface Preson{
+  userName ? : string;
+  age:number
+}
+let men:Preson = {
+  age: 78
+}
+console.error(Days['Sun'])
+console.error(men)
+let num:number[] = [787];
+console.error(num)
+let arr:[number, string] = [12,'43'];
+let arr2:any[] = [232,{},false,null,undefined]
+
+function  userFetch(){
+  const name:string = '张三';
+  const age: number = 78;
+  return tryInfo(name, age)
+}
+function tryInfo <T extends unknown[]>(...ele: T):T{
+  return ele;
+}
+let [name ] = userFetch();
+console.error(name)
